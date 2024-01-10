681cd53669224096701d2589f8c660b0996dd45c
Keegan Royal-Eisenberg <kroyal.eisenberg@gmail.com>
2024-01-09 20:07:04 -0800

    Began work on HTML, CSS, and JS. Created the timer, a start quiz button, and updated the README.

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Code_Quiz
 An application that challenges the user with a timed quiz on Javascript fundamentals.
+
+[How to hide a button after a click on Stackoverflow](https://stackoverflow.com/questions/57716269/how-to-hide-a-button-after-click)
---
+++ assets/script.js
@@ -0,0 +1,74 @@
+//show a start quiz button
+//on click, hide start button
+
+const start = (element) => {
+  element.hidden = true;
+}
+//start timer
+var timeEl = document.querySelector(".time");
+
+var GameOver = document.querySelector("GameOver");
+
+var secondsLeft = 2;
+
+function setTime() {
+  // Sets interval in variable
+  var timerInterval = setInterval(function() {
+    // As long as the `secondsLeft` is greater than 1
+    if(secondsLeft > 1) {
+      // Set the `textContent` of `timerEl` to show the remaining seconds
+      timeEl.textContent = "Timer: "+ secondsLeft + " secs.";
+      // Decrement `secondsLeft` by 1
+      secondsLeft--;
+    } else if (secondsLeft === 1) {
+      // When `timeLeft` is equal to 1, rename to 'second' instead of 'seconds'
+      timeEl.textContent = "Timer: " + secondsLeft + " sec.";
+      secondsLeft--;
+    } else {
+      // Once `secondsLeft` gets to 0, set `timerEl` to an empty string
+      timeEl.textContent = '';
+      // Use `clearInterval()` to stop the timer
+      clearInterval(timeInterval);
+      // Call the `displayMessage()` function
+      sendMessageMessage();
+    }
+
+  }, 1000);
+}
+
+// Function to display game ovr rmessage
+function sendMessage() {
+  var divText = document.createElement("p");
+  divText.setAttribute("p", "Game Over");
+  GameOver.appendChild(divText);
+}
+
+setTime();
+
+
+//show a start quiz button
+//on click, hide start button
+
+
+//show first question
+
+
+//on click of chosen answer, 
+
+//see if the answer is correct
+
+//if correct, add to score and display "Correct!"
+
+//else, don't add to score and display "Wrong!"
+
+//show next questions
+
+//repeat until quiz end or timer runs out
+
+//after last question, or timer runs out, hide questions and show score
+
+//allow saving intials and score
+
+//on click of "Show Highscores", hide quiz and show list of scores
+
+//on click of "Clear scores", clear scores
